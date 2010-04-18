101f93de291136bb8fbb5bec7820ad8453244a93
Trung Nguyen <trung@mdkt.org>
2010-04-18 16:33:51 +0800

    fix navigation

--- webui/js/riak-webui.js
+++ webui/js/riak-webui.js
@@ -5,7 +5,9 @@ $(function() {
 	$("#msg").hide();
 	$("button").button();
 	$("#dialog").dialog("destroy");
-	$("#navigation").accordion();
+	// $("#navigation").accordion();
+	slide("#sliding-navigation", 25, 15, 150, .8);
+
 	keysTable = $('#keysTable').dataTable({
 		"bJQueryUI": true,
 		"sPaginationType": "full_numbers",
@@ -105,6 +107,42 @@ $(function() {
 		}});
 	});
 });
+
+function slide(navigation_id, pad_out, pad_in, time, multiplier)
+{
+	// creates the target paths
+	var list_elements = navigation_id + " li.sliding-element";
+	var link_elements = list_elements + " a";
+	
+	// initiates the timer used for the sliding animation
+	var timer = 0;
+	
+	// creates the slide animation for all list elements 
+	$(list_elements).each(function(i)
+	{
+		// margin left = - ([width of element] + [total vertical padding of element])
+		$(this).css("margin-left","-180px");
+		// updates timer
+		timer = (timer*multiplier + time);
+		$(this).animate({ marginLeft: "0" }, timer);
+		$(this).animate({ marginLeft: "15px" }, timer);
+		$(this).animate({ marginLeft: "0" }, timer);
+	});
+
+	// creates the hover-slide effect for all link elements 		
+	$(link_elements).each(function(i)
+	{
+		$(this).hover(
+		function()
+		{
+			$(this).animate({ paddingLeft: pad_out }, 150);
+		},		
+		function()
+		{
+			$(this).animate({ paddingLeft: pad_in }, 150);
+		});
+	});
+}
 
 function delete_keys(selectedRows) {
 	var total = $(selectedRows).length;
